d6aa1c2df71725020359386dd90d75ca2428ee54
iyashraj <yashr3037@gmail.com>
2022-04-27 10:41:37 +0530

    my first commit- netflix-deply

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  
+  return (
+    <div className="navbar">
+    <img className="nav_logo" src="http://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="netflix" />
+    <img className="nav_avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Avatar" />
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = "9466f2e2c9bde6cf191bfcca0a261989";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top-rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
